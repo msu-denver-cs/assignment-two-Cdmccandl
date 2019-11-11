6967400c1ed69664806d2c2a7b0babbbe0f68070
Conor McCandless <cdmccandl@gmail.com>
2019-11-11 00:48:58 -0700

    added validations for car, makes and carspart

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :make
   has_and_belongs_to_many :parts
+
+validates :make, presence: true
+validates :vin_number, presence: true, uniqueness: true
+validates :model_title, presence: true, length: { in: 2..20 }
 end
